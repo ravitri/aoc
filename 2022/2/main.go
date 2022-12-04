c8c92cc72c0d02e286aa349d00312b08bbc14be6
Ravi Trivedi <travi@redhat.com>
2022-12-04 11:17:33 +0530

    Simplifying day 2 program

--- 2022/2/main.go
+++ 2022/2/main.go
@@ -13,39 +13,40 @@ const (
 	Win  int = 6
 )
 
-func round1(theirs string, mine string) int {
+var (
+	criteria = map[string]map[string]int{
+		"A": {
+			"X": Draw,
+			"Y": Win,
+			"Z": Loss,
+		},
+		"B": {
+			"X": Loss,
+			"Y": Draw,
+			"Z": Win,
+		},
+		"C": {
+			"X": Win,
+			"Y": Loss,
+			"Z": Draw,
+		},
+	}
 
+	score1 = map[string]int{
+		"X": 1,
+		"Y": 2,
+		"Z": 3,
+	}
+)
+
+func part1(theirs string, mine string) int {
 	points := 0
-
-	criteria := map[string]map[string]int{}
-	criteria["A"] = map[string]int{}
-	criteria["B"] = map[string]int{}
-	criteria["C"] = map[string]int{}
-	criteria["A"]["Y"] = Win
-	criteria["A"]["Z"] = Loss
-	criteria["A"]["X"] = Draw
-	criteria["B"]["X"] = Loss
-	criteria["B"]["Y"] = Draw
-	criteria["B"]["Z"] = Win
-	criteria["C"]["X"] = Win
-	criteria["C"]["Y"] = Loss
-	criteria["C"]["Z"] = Draw
-
-	score := map[string]int{}
-
-	score["X"] = 1
-	score["Y"] = 2
-	score["Z"] = 3
-
-	points = criteria[theirs][mine] + score[mine]
-
+	points = criteria[theirs][mine] + score1[mine]
 	return points
 }
 
-func round2(theirs string, mine string) int {
-
+func part2(theirs string, mine string) int {
 	points := 0
-
 	replace := map[string]map[string]string{}
 	replace["A"] = map[string]string{}
 	replace["B"] = map[string]string{}
@@ -60,45 +61,25 @@ func round2(theirs string, mine string) int {
 	replace["C"]["Y"] = "Z"
 	replace["C"]["Z"] = "X"
 
-	criteria := map[string]map[string]int{}
-	criteria["A"] = map[string]int{}
-	criteria["B"] = map[string]int{}
-	criteria["C"] = map[string]int{}
-	criteria["A"]["Y"] = Win
-	criteria["A"]["Z"] = Loss
-	criteria["A"]["X"] = Draw
-	criteria["B"]["X"] = Loss
-	criteria["B"]["Y"] = Draw
-	criteria["B"]["Z"] = Win
-	criteria["C"]["X"] = Win
-	criteria["C"]["Y"] = Loss
-	criteria["C"]["Z"] = Draw
-
-	score := map[string]int{}
-
-	score["X"] = 1
-	score["Y"] = 2
-	score["Z"] = 3
-
-	points = criteria[theirs][replace[theirs][mine]] + score[replace[theirs][mine]]
+	points = criteria[theirs][replace[theirs][mine]] + score1[replace[theirs][mine]]
 
 	return points
 }
 
 func rps(input []string) (int, int) {
 
-	score := 0
 	score1 := 0
+	score2 := 0
 
 	for _, play := range input {
 		p := strings.Split(play, " ")
-		s := round1(p[0], p[1])
-		s1 := round2(p[0], p[1])
-		score += s
+		s1 := part1(p[0], p[1])
+		s2 := part2(p[0], p[1])
 		score1 += s1
+		score2 += s2
 	}
 
-	return score, score1
+	return score1, score2
 }
 
 func main() {
